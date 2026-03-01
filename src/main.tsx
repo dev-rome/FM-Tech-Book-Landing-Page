a506bcfc982d3baf11804ac745056d46821c7fe0
dev-rome <info.devrome@gmail.com>
2026-03-01 15:33:12 -0500

    add Martian Mono and Inter font imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "@fontsource/martian-mono/400.css";
+import "@fontsource/martian-mono/600.css";
+import "@fontsource/martian-mono/700.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/600.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);

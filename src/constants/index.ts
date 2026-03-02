b1ca2a9aa13dec16c1670de61d3ba15583ef62eb
dev-rome <info.devrome@gmail.com>
2026-03-01 19:19:06 -0500

    add about list items constant

---
+++ src/constants/index.ts
@@ -0,0 +1,18 @@
+export const ABOUT_LIST_ITEMS = [
+  {
+    id: 1,
+    text: "Monthly curated tech reads selected by industry experts",
+  },
+  {
+    id: 2,
+    text: "Virtual and in-person meetups for deep-dive discussions",
+  },
+  {
+    id: 3,
+    text: "Early access to new tech book releases",
+  },
+  {
+    id: 4,
+    text: "Author Q&A sessions with tech thought leaders",
+  },
+];

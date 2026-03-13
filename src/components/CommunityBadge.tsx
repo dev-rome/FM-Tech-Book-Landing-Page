26e3fd8890198177ee39f19b8b565dca885d2bee
dev-rome <info.devrome@gmail.com>
2026-03-13 17:59:41 -0400

    added className props to CommunityBadge

--- src/components/CommunityBadge.tsx
+++ src/components/CommunityBadge.tsx
@@ -1,22 +1,17 @@
 import Avatars from "/images/image-avatars.webp";
 import Star from "../svg/Star";
 
-function CommunityBadge() {
+function CommunityBadge({ className }: { className?: string }) {
   return (
-    <div className="flex items-center gap-3">
+    <div className={`flex items-center gap-3 ${className ?? ""}`}>
       <img src={Avatars} alt="Community avatars" className="h-10 w-28" />
       <div className="flex flex-col gap-1">
         <div className="flex">
           {Array.from({ length: 5 }).map((_, i) => (
-            <Star
-              key={i}
-              className="h-6 w-6"
-            />
+            <Star key={i} className="h-6 w-6" />
           ))}
         </div>
-        <p className="font-martian text-14 text-neutral-700">
-          200+ developers joined already
-        </p>
+        <p className="font-martian text-14">200+ developers joined already</p>
       </div>
     </div>
   );
